4986fd2f88c9661a7e3922ba66e1e8c87400c022
GuilhermeVeigaPedromilo <125681788+GuilhermeVeigaPedromilo@users.noreply.github.com>
2023-07-28 11:00:07 -0300

    Atualização

--- imc.js
+++ imc.js
@@ -9,29 +9,25 @@
             ano = parseFloat(document.getElementById("ano").value);
 
             nome = document.getElementById("nome").value;
-            
-            idade = 2023-ano;
+
+            idade = ano
 
             console.log(ano);
 
-            if (idade <= -0.1) {
+           
+            if (idade >= 1 && idade <= 11) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br>" + nome + "<br/> Você nasceu no futuro?";
-            } else if (idade >= 1 && idade <= 12) {
-                resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Criança";
+                resultado.innerHTML = "<br/>" + nome + "<br/> A pessoa é criança";
             } else if (idade >= 12 && idade <= 21) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Adolescente";
+                resultado.innerHTML = "<br/>" + nome + "<br/> A pessoa é adolescente";
             } else if (idade >= 21 && idade <= 65) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Adulto";
-            } else if (idade >= 65 && idade <= 130) {
+                resultado.innerHTML = "<br/>" + nome + "<br/> A pessoa é adulto";
+            } else if (idade >= 65) {
                 resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Você está na fase Idoso";
-			} else if (idade >= 130) {
-                resultado = document.getElementById("resultado");
-                resultado.innerHTML = "<br/>" + nome + " sua idade é " + idade + "<br/> Pode ir para um museu";
+                resultado.innerHTML = "<br/>" + nome + "<br/> A pessoa é idoso";
+			
         }
            
     }
